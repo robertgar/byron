48f1111bcec97113713e76679d8a6d067c67a4c6
robertgar <50602861+robertgar@users.noreply.github.com>
2019-08-12 18:12:47 -0600

    Update casa.php

--- casa.php
+++ casa.php
@@ -8,7 +8,9 @@ ON inmueble.est_fk = tipo_estado.id_esta WHERE `tip_in_fk`=2";
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Inmuebles</title>
+   <title> Inmuebles</title> 
+        <!-- add icon link -->
+        <link rel = "icon" href ="im/logo.jpeg" type = "image/x-icon">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
