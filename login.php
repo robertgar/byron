8d677cf4787cc35ad076ed6b102a5a2b4f4a7b18
robertgar <50602861+robertgar@users.noreply.github.com>
2019-08-12 18:13:59 -0600

    Update login.php

--- login.php
+++ login.php
@@ -11,7 +11,9 @@ if(isset($_SESSION['use']))
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Inmuebles</title>
+		  <title> Inmuebles</title> 
+        <!-- add icon link -->
+        <link rel = "icon" href ="im/logo.jpeg" type = "image/x-icon">
 		<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1"/>
 		   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 	</head>
